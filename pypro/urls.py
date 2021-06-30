677dfab3bc52168bf999d23730dc820431ee6227
viniciusfrantz <viniciusfrantz@hotmail.com>
2021-06-30 18:39:38 -0300

    testes heroku

--- pypro/urls.py
+++ pypro/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from Scripts.activate_this import path
+
+from pypro.base import admin
+
+...
+
+from pypro.base.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
 ]
