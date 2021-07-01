67223b1652311c5afc8d90184a3ea6a6b6f12bb2
viniciusfrantz <viniciusfrantz@hotmail.com>
2021-07-01 08:40:03 -0300

    teste site 'olá django'

--- pypro/urls.py
+++ pypro/urls.py
@@ -1,3 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+
+from pypro.base.views import home
+
 """pypro URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,15 +18,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from Scripts.activate_this import path
-
-from pypro.base import admin
-
-...
-
-from pypro.base.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+
+
 ]
